2adb4debc37c09cec4fc72e33a16edccf8eeca42
Tony G <tony1427@msn.com>
2022-11-11 21:34:10 -0600

    Initial commit

---
+++ src/main/kotlin/com/walnutvw/vehicle/VehicleApplication.kt
@@ -0,0 +1,11 @@
+package com.walnutvw.vehicle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleApplication
+
+fun main(args: Array<String>) {
+	runApplication<VehicleApplication>(*args)
+}
---
+++ src/test/kotlin/com/walnutvw/vehicle/VehicleApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.walnutvw.vehicle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class VehicleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
